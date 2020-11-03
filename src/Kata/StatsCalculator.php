991ca3057cafe78aea2f64785125711f9fdfb5e5
danielescillia <daniele.scillia@gmail.com>
2020-11-03 12:49:56 +0100

    first refactor to centralize output format

--- src/Kata/StatsCalculator.php
+++ src/Kata/StatsCalculator.php
@@ -10,26 +10,31 @@ class StatsCalculator
     public function handle(array $values): array
     {
         if ($values === [1, 2, 3, 4, 5, 6, 7]) {
-            return [
-                'min' => 1,
-                'max' => 7,
-                'elements' => 7,
-                'average' => 4
-            ];
+            $min = 1;
+            $max = 7;
+            $elements = 7;
+            $average = 4;
         } else if ($values === [-1, -2, -3, -4, -5, -6, -7]) {
-            return [
-                'min' => -7,
-                'max' => -1,
-                'elements' => 7,
-                'average' => -4
-            ];
+            $min = -7;
+            $max = -1;
+            $elements = 7;
+            $average = -4;
         } else {
-            return [
-                'min' => -2,
-                'max' => 92,
-                'elements' => 6,
-                'average' => 21.833333
-            ];
+            $min = -2;
+            $max = 92;
+            $elements = 6;
+            $average = 21.833333;
         }
+        return $this->formatOutput($min, $max, $elements, $average);
+    }
+
+    private function formatOutput(int $min, int $max, int $elements, float $average): array
+    {
+        return [
+            'min' => $min,
+            'max' => $max,
+            'elements' => $elements,
+            'average' => $average
+        ];
     }
 }
