127113006189756d4566db1a70603152b3816d69
danielescillia <daniele.scillia@gmail.com>
2020-11-03 12:51:41 +0100

    abstraction of min logic

--- src/Kata/StatsCalculator.php
+++ src/Kata/StatsCalculator.php
@@ -10,22 +10,27 @@ class StatsCalculator
     public function handle(array $values): array
     {
         if ($values === [1, 2, 3, 4, 5, 6, 7]) {
-            $min = 1;
+            $min = $this->min($values);
             $max = 7;
             $elements = 7;
             $average = 4;
         } else if ($values === [-1, -2, -3, -4, -5, -6, -7]) {
-            $min = -7;
+            $min = $this->min($values);
             $max = -1;
             $elements = 7;
             $average = -4;
         } else {
-            $min = -2;
+            $min = $this->min($values);
             $max = 92;
             $elements = 6;
             $average = 21.833333;
         }
         return $this->formatOutput($min, $max, $elements, $average);
+    }
+
+    private function min(array $values): int
+    {
+        return min($values);
     }
 
     private function formatOutput(int $min, int $max, int $elements, float $average): array
